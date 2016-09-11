89ab7d935f136c68e053ebe97558d2482455b5ae
Phelim Bradley <wave@phel.im>
2016-09-11 10:40:08 +0100

    test set opperations

--- redispartition/client.py
+++ redispartition/client.py
@@ -26,6 +26,38 @@ class RedisCluster(object):
     def getbit(self, k, i, conn=None):
         return conn.getbit(k, i)
 
+    @pipeiflist
+    def rpush(self, k, i, conn=None):
+        return conn.rpush(k, i)
+
+    @pipeiflist
+    def incr(self, *args, conn=None, **kwargs):
+        return conn.incr(*args, **kwargs)
+
+    @pipeiflist
+    def sadd(self, k, v, conn=None):
+        return conn.sadd(k, v)
+
+    @pipeiflist
+    def srem(self, k, v, conn=None):
+        return conn.srem(k, v)
+
+    @pipeiflist
+    def sismember(self, k, v, conn=None):
+        return conn.sismember(k, v)
+
+    def calculate_memory(self):
+        return sum(r.info().get('used_memory') for r in self.connections)
+
+    def delete(self):
+        [r.flushall() for r in self.connections]
+
+    def shutdown(self):
+        [r.shutdown() for r in self.connections]
+
+    def dbsize(self):
+        return sum([r.dbsize() for r in self.connections])
+
     def get_connection(self, k):
         if isinstance(k, str):
             k = str.encode(k)
--- redispartition/tests/test_cluster.py
+++ redispartition/tests/test_cluster.py
@@ -39,6 +39,7 @@ def test_choose_connection_bytes(i):
 @given(c=text(min_size=1), _bytes=binary(min_size=1))
 def test_set_get_text(c, _bytes):
     rhd = RedisCluster(connections)
+    rhd.delete()
     rhd.set(c, _bytes)
     assert rhd.get(c) == _bytes
 
@@ -46,6 +47,7 @@ def test_set_get_text(c, _bytes):
 @given(c=st.lists(text(min_size=1), unique=True, max_size=5, min_size=5), _bytes=st.lists(binary(min_size=1), unique=True, max_size=5, min_size=5))
 def test_set_get_pipe(c, _bytes):
     rhd = RedisCluster(connections)
+    rhd.delete()
     rhd.set(c, _bytes)
     assert rhd.get(c) == _bytes
 
@@ -53,6 +55,7 @@ def test_set_get_pipe(c, _bytes):
 @given(c=integers(), _bytes=binary(min_size=1))
 def test_set_get_int(c, _bytes):
     rhd = RedisCluster(connections)
+    rhd.delete()
     rhd.set(c, _bytes)
     assert rhd.get(c) == _bytes
 
@@ -60,6 +63,7 @@ def test_set_get_int(c, _bytes):
 @given(c=text(min_size=1), i=integers(min_value=0, max_value=100000), bit=integers(min_value=0, max_value=1))
 def test_setbit_getbit_int(c, i, bit):
     rhd = RedisCluster(connections)
+    rhd.delete()
     rhd.setbit(c, i, bit)
     assert rhd.getbit(c, i) == bit
 
@@ -67,5 +71,32 @@ def test_setbit_getbit_int(c, i, bit):
 @given(c=st.lists(text(min_size=1), unique=True, max_size=5, min_size=5), i=st.lists(integers(min_value=0, max_value=100000), max_size=5, min_size=5), bits=st.lists(integers(min_value=0, max_value=1), max_size=5, min_size=5))
 def test_setbit_getbit_pipe(c, i, bits):
     rhd = RedisCluster(connections)
+    rhd.delete()
     rhd.setbit(c, i, bits)
     assert rhd.getbit(c, i) == bits
+
+
+@given(c=text(min_size=1), v=text(min_size=1))
+def test_sadd_sismember_text(c, v):
+    rhd = RedisCluster(connections)
+    rhd.delete()
+    rhd.sadd(c, v)
+    assert rhd.sismember(c, v)
+
+
+@given(c=st.lists(text(min_size=1), unique=True, max_size=5, min_size=5), v=st.lists(text(min_size=1), unique=True, max_size=5, min_size=5))
+def test_sadd_sismember_text_pipe(c, v):
+    rhd = RedisCluster(connections)
+    rhd.delete()
+    rhd.sadd(c, v)
+    assert all(rhd.sismember(c, v))
+
+
+@given(c=text(min_size=1), v=text(min_size=1))
+def test_srem_sismember_text(c, v):
+    rhd = RedisCluster(connections)
+    rhd.delete()
+    rhd.sadd(c, v)
+    assert rhd.sismember(c, v)
+    rhd.srem(c, v)
+    rhd.sismember(c, v) is False
